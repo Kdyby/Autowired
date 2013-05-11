9d6938e8bf63226dfe68c8d6244e5d619fa06d57
Filip Procházka <filip@prochazka.su>
2013-05-11 15:46:07 +0200

    typo

--- src/Kdyby/Autowired/AutowireComponentFactories.php
+++ src/Kdyby/Autowired/AutowireComponentFactories.php
@@ -55,9 +55,9 @@ trait AutowireComponentFactories
 	 */
 	public function injectComponentFactories(Nette\DI\Container $dic)
 	{
-//		if (!$this instanceof Nette\Application\UI\PresenterComponent) {
-//			throw new MemberAccessException('Trait ' . __TRAIT__ . ' can be used only in descendants of PresenterComponent.');
-//		}
+		if (!$this instanceof Nette\Application\UI\PresenterComponent) {
+			throw new MemberAccessException('Trait ' . __TRAIT__ . ' can be used only in descendants of PresenterComponent.');
+		}
 
 		$this->autowireComponentFactoriesLocator = $dic;
 		$cache = new Nette\Caching\Cache($dic->getByType('Nette\Caching\IStorage'), 'Kdyby.Autowired.AutowireComponentFactories');
