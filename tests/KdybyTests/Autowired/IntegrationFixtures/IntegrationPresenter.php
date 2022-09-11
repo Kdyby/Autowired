64826737c0571c878bfb14962145c2d441e137f3
Petr Morávek <petr@pada.cz>
2022-09-11 09:06:55 +0200

    Deprecate phpdoc annotations in favor of native PHP attributes

--- tests/KdybyTests/Autowired/IntegrationFixtures/IntegrationPresenter.php
+++ tests/KdybyTests/Autowired/IntegrationFixtures/IntegrationPresenter.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace KdybyTests\Autowired\IntegrationFixtures;
 
 use Kdyby;
+use Kdyby\Autowired\Attributes\Autowire;
 use Nette;
 
 class IntegrationPresenter extends Nette\Application\UI\Presenter
@@ -12,14 +13,10 @@ class IntegrationPresenter extends Nette\Application\UI\Presenter
 	use Kdyby\Autowired\AutowireProperties;
 	use Kdyby\Autowired\AutowireComponentFactories;
 
-	/**
-	 * @autowire
-	 */
+	#[Autowire]
 	public LoremService $service;
 
-	/**
-	 * @autowire(factory=\KdybyTests\Autowired\IntegrationFixtures\DatagridFactory)
-	 */
+	#[Autowire(factory: DatagridFactory::class)]
 	public DatagridComponent $factoryResult;
 
 	protected function createComponentSilly(DatagridFactory $factory): DatagridComponent
